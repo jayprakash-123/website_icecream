6e6aff2cfb169cb6a18322fa40f31581cd118edc
Jay <jayprakash15cs22@gmail.com>
2021-08-03 23:09:02 +0530

    update code

--- home/views.py
+++ home/views.py
@@ -17,8 +17,6 @@ def about(request):
 
 def services(request):
     return render(request, 'services.html')
- 
-
 def contact(request):
     if request.method == "POST":
         name = request.POST.get('name')
